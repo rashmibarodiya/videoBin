5021ea88f65f3059f69f1230ea521ef2cc17c32b
rashmibarodiya <rashmi.kht0@gmail.com>
2025-07-17 17:01:47 +0530

    nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 
-import { authOption } from "@/lib/auth";
+import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
 
-const handler = NextAuth(authOption)
+const handler = NextAuth(authOptions)
 
 export {handler as GET, handler as POST}
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,63 @@
 import { NextAuthOptions } from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connect } from "./connect"
+import User from "@/models/user"
+import bcrypt from "bcryptjs";
 
-export const authOption: NextAuthOptions = {
-    providers:[
-       
-    ]
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "Email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("Email and Password are required")
+                }
+                try {
+                    await connect()
+                    let user = await User.findOne({ email: credentials.email })
+                    if (!user) {
+                        throw new Error("No such user found with this email")
+                    }
+                    const valid = await bcrypt.compare(credentials.password, user.password);
+
+                    if (!valid) {
+                        throw new Error("Invalid password!")
+                    }
+
+                    return {
+                        id: user._id.toString(),
+                        email: user.email
+                    }
+
+                } catch (error) {
+                    console.error("Error while logging in ", error)
+                    throw error
+                }
+            }
+        })
+    ], callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user.id = token.id as string
+            }
+            return session
+        }
+    }, pages: {
+        signIn: "/login",
+        error: "/login"
+    }, session: { 
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60
+    }, secret: process.env.AUTH_SECRET
 }
